f1915e18270be0220859dbbccbc7eec384757bc8
Jiehong WANG <jiehong.wang@halliburton.com>
2021-07-05 11:26:14 +0800

    update tcp

--- projects/page07022021/src/lib/page07022021-routing.module.ts
+++ projects/page07022021/src/lib/page07022021-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page07022021Component } from './page07022021.component';
 
-
 const routes: Routes = [
   { path: '', component: Page07022021Component }
 ];
